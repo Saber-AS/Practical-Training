0f5316080a43b93a37d668aae1efff322f25b37d
Saber-AS <2388348901@qq.com>
2024-07-08 11:23:16 +0800

    实训配置maven

---
+++ src/main/java/com/swpu_pt/www/swpu_pt/SwpuPtApplication.java
@@ -0,0 +1,13 @@
+package com.swpu_pt.www.swpu_pt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwpuPtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwpuPtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swpu_pt/www/swpu_pt/SwpuPtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swpu_pt.www.swpu_pt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwpuPtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
